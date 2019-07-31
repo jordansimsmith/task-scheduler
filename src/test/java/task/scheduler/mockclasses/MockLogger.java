fbfed61d5a460fe770b2cad833626a698d1dd9dd
Harrison Leach <HarrisonL7190@gmail.com>
2019-07-31 18:33:44 +1200

    Add MockLogger class to allow for message assertions in unit tests

---
+++ src/test/java/task/scheduler/mockclasses/MockLogger.java
@@ -0,0 +1,25 @@
+package task.scheduler.mockclasses;
+
+import task.scheduler.ILogger;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A MockLogger stores logged items internally to be asserted
+ */
+public class MockLogger implements ILogger {
+    private List<String> loggedItems;
+
+    public MockLogger(){
+        loggedItems = new ArrayList<>();
+    }
+
+    @Override
+    public void log(String message) {
+        loggedItems.add(message);
+    }
+
+    public List<String> getLoggedItems() {
+        return loggedItems;
+    }
+}
