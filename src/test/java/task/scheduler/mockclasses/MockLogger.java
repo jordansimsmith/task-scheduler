bb72a17dc2a38628bb85f0e353c36ec023e5d39b
Harrison Leach <HarrisonL7190@gmail.com>
2019-08-01 07:01:46 +1200

    Add clear method to MockLogger class, so it can be reused and reset between unit tests

--- src/test/java/task/scheduler/mockclasses/MockLogger.java
+++ src/test/java/task/scheduler/mockclasses/MockLogger.java
@@ -22,4 +22,8 @@ public class MockLogger implements ILogger {
     public List<String> getLoggedItems() {
         return loggedItems;
     }
+
+    public void clearLoggedItems(){
+        loggedItems.clear();
+    }
 }
