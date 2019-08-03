091223dcd34831bdde14a9d3c4ddd07e0d9ec697
Jed Robertson <jrob928@aucklanduni.ac.nz>
2019-08-03 12:59:32 +1200

    Added first tests for graph loading

---
+++ src/test/java/task/scheduler/TestGraphLoading.java
@@ -0,0 +1,49 @@
+package task.scheduler;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import task.scheduler.exception.DotFormatException;
+
+import java.io.File;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestGraphLoading {
+    private String dotFiles = "src/test/resources/dot_files/";
+
+    @Test
+    public void testValidNoComments() throws Exception {
+        // arrange
+        String file = "valid_no_comments.dot";
+
+        // act
+        IGraph g = new Graph(new File(dotFiles+file));
+
+        // assert
+        assertEquals(g.getNodeCount(), 4);
+        assertEquals(g.getStartNode().getLabel(), "a");
+        assertEquals(g.getEndNode().getLabel(), "d");
+
+        for (INode node : g.getNodes()) {
+            if (node.getLabel().equals("b")) {
+                assertEquals(1, node.getParents().size());
+                assertEquals("a", node.getParents().get(0).x.getLabel());
+                assertEquals(1, (int) node.getParents().get(0).y);
+
+                assertEquals(1, node.getChildren().size(), 1);
+                assertEquals("d", node.getChildren().get(0).x.getLabel());
+                assertEquals(2, (int) node.getChildren().get(0).y);
+            }
+        }
+    }
+
+    @Test(expected = DotFormatException.class)
+    public void testLoopedGraph() throws Exception {
+        // arrange
+        String file = "invalid_looped.dot";
+
+        // act
+        IGraph g = new Graph(new File(dotFiles+file));
+    }
+}
