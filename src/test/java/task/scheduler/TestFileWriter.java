b128b3be6a84c3b917804cc5486229d4ab0ad753
Harrison Leach <HarrisonL7190@gmail.com>
2019-08-04 13:38:43 +1200

    Create TestFileWriter and write single unit test for Testing the FileWriter - will add some more unit tests soon

---
+++ src/test/java/task/scheduler/TestFileWriter.java
@@ -0,0 +1,78 @@
+package task.scheduler;
+
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestFileWriter {
+
+    private FileWriter fileWriter;
+    @Spy  private ByteArrayOutputStream outputStream;
+    @Mock private IGraph mockGraph;
+    @Mock private ISchedule mockSchedule;
+
+    public TestFileWriter(){
+        outputStream = new ByteArrayOutputStream();
+    }
+
+    @Before
+    public void setUp() {
+        this.fileWriter = new FileWriter(outputStream);
+    }
+
+    @After
+    public void tearDown() {
+        Config config = Config.getInstance();
+
+        // clear output stream between tests
+        outputStream.reset();
+
+        // reset singleton config object between tests
+        config.setInputFile(null);
+        config.setOutputFile(null);
+        config.setNumberOfCores(0);
+        config.setNumberOfThreads(0);
+        config.setVisualise(false);
+    }
+
+    @Test
+    public void testSingleNode() {
+        // arrange
+        String expectedString = "digraph \"testSingleNode\" {\n" +
+                "\ta\t[Weight=1 Start=2 Processor=3];\n" +
+                "}";
+
+        Node node = new Node(1, "a");
+        List<INode> nodes = new ArrayList<>();
+        nodes.add(node);
+
+        when(mockGraph.getNodes()).thenReturn(nodes);
+        when(mockSchedule.getNodeSchedule(node)).thenReturn(new Tuple<>(2,3));
+
+        // act
+        fileWriter.writeScheduledGraphToFile(mockGraph, mockSchedule, "testSingleNode");
+
+        // remove carriage return if testing on windows
+        String actualString = outputStream.toString().replaceAll("\r","");
+
+        // assert
+        assertEquals(expectedString, actualString);
+        verify(outputStream, atLeastOnce()).write((byte[]) Mockito.any(), Mockito.anyInt(), Mockito.anyInt());
+    }
+}
+
