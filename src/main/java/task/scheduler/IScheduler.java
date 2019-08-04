fb9ffa153e3ddc4ae477426f439f2636b4b2f15f
res550 <37566536+res550@users.noreply.github.com>
2019-08-04 16:03:16 +1200

    Defining the interface for schedulers

--- src/main/java/task/scheduler/IScheduler.java
+++ src/main/java/task/scheduler/IScheduler.java
@@ -2,9 +2,14 @@ package task.scheduler;
 
 
 /**
- * Interface to define what each scheduler will need to have
+ * Interface to define what each scheduler will need to have.
  * */
 public interface IScheduler {
 
+    /**
+     * Each scheduler will need an execute method which will be responsible for returning the schedule
+     *
+     * @return Schedule which is built according to the scheduling algorithm
+     * */
     ISchedule execute();
 }
