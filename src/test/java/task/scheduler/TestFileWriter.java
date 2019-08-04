13cf7b3ae5a059b8662a4001c16256d53cc11e31
Harrison Leach <HarrisonL7190@gmail.com>
2019-08-04 16:06:04 +1200

    Write unit test to verify FileWriter can write a tree graph

--- src/test/java/task/scheduler/TestFileWriter.java
+++ src/test/java/task/scheduler/TestFileWriter.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.atLeastOnce;
@@ -64,6 +65,54 @@ public class TestFileWriter {
 
         when(mockGraph.getNodes()).thenReturn(nodes);
         when(mockSchedule.getNodeSchedule(node)).thenReturn(new Tuple<>(2,3));
+
+        // act
+        fileWriter.writeScheduledGraphToFile(mockGraph, mockSchedule);
+
+        // remove carriage return if testing on windows
+        String actualString = outputStream.toString().replaceAll("\r","");
+
+        // assert
+        assertEquals(expectedString, actualString);
+        verify(outputStream, atLeastOnce()).write((byte[]) Mockito.any(), Mockito.anyInt(), Mockito.anyInt());
+    }
+
+    @Test
+    public void testTreeOfDepthOne() throws IOException {
+        // arrange
+        config.setOutputFile(new File("src/testTree.dot"));
+        StringBuilder stringBuilder = new StringBuilder("digraph \"testTree\" {\n");
+        stringBuilder.append("\t0\t[Weight=1 Start=0 Processor=0];\n");
+
+        // set up single parent node
+        Node firstNode = new Node(1, "0");
+        List<INode> nodes = new ArrayList<>();
+        nodes.add(firstNode);
+        when(mockSchedule.getNodeSchedule(firstNode)).thenReturn(new Tuple<>(0, 0));
+
+        int[] array = new int[50];
+
+        for (int i = 1; i < array.length; i++){
+            array[i] = i;
+        }
+
+        // attach children nodes
+        for (int i : array) {
+            Node node = new Node(i, String.valueOf(i));
+            when(mockSchedule.getNodeSchedule(node)).thenReturn(new Tuple<>(i, i));
+            stringBuilder.append(String.format("\t%s\t[Weight=%d Start=%d Processor=%d];\n", node.getLabel(), i, i, i));
+
+            node.addDependency(firstNode, i);
+            stringBuilder.append(String.format("\t%s -> %s\t[Weight=%d];\n", firstNode.getLabel(), node.getLabel(), i));
+
+            nodes.add(node);
+        }
+
+        when(mockGraph.getNodes()).thenReturn(nodes);
+
+        stringBuilder.append("}");
+        String expectedString = stringBuilder.toString();
+
 
         // act
         fileWriter.writeScheduledGraphToFile(mockGraph, mockSchedule);
