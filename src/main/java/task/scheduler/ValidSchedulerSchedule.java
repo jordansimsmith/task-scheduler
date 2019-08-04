8085e021f45debdce2275694e21d7d5a6bab9cb7
res550 <37566536+res550@users.noreply.github.com>
2019-08-04 16:05:55 +1200

    Adding a basic schedule

---
+++ src/main/java/task/scheduler/ValidSchedulerSchedule.java
@@ -0,0 +1,30 @@
+package task.scheduler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+/**
+ *  Schedule class for the Valid Algorithm.
+ * */
+public class ValidSchedulerSchedule implements ISchedule{
+    private Map<INode,Tuple<Integer,Integer>> schedule;
+    private int cost = 0;
+
+    public ValidSchedulerSchedule(){
+        this.schedule = new HashMap<>();
+    }
+
+    public void addSchedule(INode node, int startTime, int processor){
+        schedule.put(node,new Tuple<Integer,Integer>(processor,startTime));
+    }
+
+    public int getScheduleSize(){
+        return schedule.size();
+    }
+
+    @Override
+    public Tuple<Integer, Integer> getNodeSchedule(INode node) {
+        return this.schedule.get(node);
+    }
+}
