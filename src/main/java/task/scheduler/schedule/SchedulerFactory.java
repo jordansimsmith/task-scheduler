cc04f311d1fdd1acb18baa02fdc9dda43dff8290
Nidhinesh <nndihinesh@gmail.com>
2019-08-06 11:44:47 +1200

    Added SchedularFactory class

---
+++ src/main/java/task/scheduler/schedule/SchedulerFactory.java
@@ -0,0 +1,42 @@
+package task.scheduler.schedule;
+
+import task.scheduler.IScheduler;
+import task.scheduler.ValidScheduler;
+
+/**
+ * The class SchedularFactory returns a scheduler object as specified by the SchedulerType input to createSchedular
+ * method
+ */
+public class SchedulerFactory {
+
+    public enum SchedulerType {
+        VALID
+    }
+
+    public SchedulerFactory() {
+    }
+
+    /**
+     * ISchedular takes in a SchedularType and returns a corresponding scheduler.
+     *
+     * @param type
+     * @return IScheduler
+     */
+
+    public IScheduler createScheduler(SchedulerType type) {
+        if (type == null) {
+            throw new IllegalArgumentException("Null is not a valid argument");
+        }
+        switch (type) {
+            case VALID:
+                return new ValidScheduler();
+
+            default:
+                throw new RuntimeException("createSchedular case not implemented for " + type);
+
+        }
+    }
+
+}
+
+
