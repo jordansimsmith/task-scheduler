0266176fd8ffee1811bcfecba36f7427162abe72
Jed Robertson <jrob928@aucklanduni.ac.nz>
2019-08-07 15:49:39 +1200

    Added more tests for graph loading

--- src/test/java/task/scheduler/TestGraphLoading.java
+++ src/test/java/task/scheduler/TestGraphLoading.java
@@ -27,6 +27,9 @@ public class TestGraphLoading {
         mockLogger.clearLoggedItems();
     }
 
+    /**
+     * Check a basic valid graph is loaded correctly
+     */
     @Test
     public void testValidNoComments() throws Exception {
         // arrange
@@ -47,7 +50,7 @@ public class TestGraphLoading {
 
                 assertTrue(!node.getParents().containsValue(1));
 
-                assertEquals(1, node.getChildren().size(), 1);
+                assertEquals(1, node.getChildren().size());
                 assertTrue(node.getChildren().containsValue(2));
 
                 assertTrue(!node.getChildren().containsValue(3));
@@ -55,6 +58,9 @@ public class TestGraphLoading {
         }
     }
 
+    /**
+     * Checks the interpreter correctly throws an error when a looped graph is loaded.
+     */
     @Test(expected = DotFormatException.class)
     public void testLoopedGraph() throws Exception {
         // arrange
@@ -65,5 +71,62 @@ public class TestGraphLoading {
 
         // assert - should also throw exception
         assertTrue(mockLogger.getLoggedItems().isEmpty());
+    }
+
+    /**
+     * Tests a graph with strange names (containing underscores, capitals and numbers as per spec)
+     */
+    @Test
+    public void testValidSpecialNames() throws Exception {
+        // arrange
+        String file = "valid_special_names.dot";
+
+        // act
+        IGraph g = new Graph(new File(dotFiles + file), mockLogger);
+
+        // assert
+        assertEquals(g.getNodeCount(), 4);
+        assertEquals(g.getStartNode().getLabel(), "apache_node");
+
+
+        for (INode node : g.getNodes()) {
+            if (node.getLabel().equals("D322")) {
+                assertEquals(2, node.getParents().size());
+                assertTrue(node.getParents().containsValue(21));
+
+                assertEquals(0, node.getChildren().size());
+            }
+        }
+    }
+
+    /**
+     * Tests with extra params apart from Weight= for a node/edges.
+     */
+    @Test
+    public void testValidExtraParams() throws Exception {
+        // arrange
+        String file = "valid_extra_params.dot";
+
+        // act
+        IGraph g = new Graph(new File(dotFiles + file), mockLogger);
+
+        // assert
+        assertEquals(g.getNodeCount(), 4);
+        assertEquals(g.getStartNode().getLabel(), "a");
+
+
+        for (INode node : g.getNodes()) {
+            if (node.getLabel().equals("b")) {
+                assertEquals(1, node.getParents().size());
+                assertTrue(node.getParents().containsValue(11));
+
+                assertTrue(!node.getParents().containsValue(1));
+
+                assertEquals(1, node.getChildren().size());
+                assertTrue(node.getChildren().containsValue(2));
+
+                assertTrue(!node.getChildren().containsValue(3));
+            }
+        }
     }
 }
