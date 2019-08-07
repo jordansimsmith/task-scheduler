c8fd9ab83782101ac082e5044ed79fc955e68aa5
res550 <37566536+res550@users.noreply.github.com>
2019-08-07 14:33:26 +1200

    Finishing off basic implementation of AStar

--- src/main/java/task/scheduler/schedule/astar/AStar.java
+++ src/main/java/task/scheduler/schedule/astar/AStar.java
@@ -1,23 +1,42 @@
 package task.scheduler.schedule.astar;
 
+import task.scheduler.common.Config;
 import task.scheduler.graph.IGraph;
+import task.scheduler.graph.INode;
 import task.scheduler.schedule.ISchedule;
 import task.scheduler.schedule.IScheduler;
 
 import java.util.Comparator;
+import java.util.List;
 import java.util.PriorityQueue;
 
 public class AStar implements IScheduler {
 
     PriorityQueue<AStarSchedule> solutions;
 
-    public AStar(Comparator comparator){
-        this.solutions = new PriorityQueue();
+    public AStar(Comparator comparator) {
+        this.solutions = new PriorityQueue<>(new AStarBaseHeuristic());
     }
 
     @Override
-    public ISchedule execute(IGraph graph){
-
+    public ISchedule execute(IGraph graph) {
+        AStarSchedule schedule = new AStarSchedule();
+        schedule.scheduleNode(graph.getStartNode(), 1);
+        this.solutions.add(schedule);
+        while (true) {
+            AStarSchedule current = this.solutions.poll();
+            if(current.getNodesScheduled() == graph.getNodeCount()){
+                return current;
+            }
+            List<INode> nodes = current.getSchedulable();
+            for (INode node : nodes) {
+                for (int i = 1; i <= Config.getInstance().getNumberOfCores(); i++) {
+                    AStarSchedule toSchedule = new AStarSchedule(schedule.getScheduled(), schedule.getSchedulable(), schedule.getEarliestTimes(),schedule.getCost());
+                    toSchedule.scheduleNode(node, i);
+                    this.solutions.add(toSchedule);
+                }
+            }
+        }
     }
 
 }
--- src/main/java/task/scheduler/schedule/astar/AStarSchedule.java
+++ src/main/java/task/scheduler/schedule/astar/AStarSchedule.java
@@ -16,6 +16,8 @@ public class AStarSchedule implements ISchedule {
     private Map<INode, Tuple<Integer, Integer>> scheduled;
 
     private int[] earliestTimes;
+    private int cost=0;
+    private int nodesScheduled = 0;
 
     public AStarSchedule() {
         this.schedulable = new ArrayList<>();
@@ -23,10 +25,12 @@ public class AStarSchedule implements ISchedule {
         this.earliestTimes = new int[Config.getInstance().getNumberOfCores()];
     }
 
-    public AStarSchedule(Map<INode, Tuple<Integer, Integer>> map, List<INode> list, int[] times) {
+    public AStarSchedule(Map<INode, Tuple<Integer, Integer>> map, List<INode> list, int[] times, int cost) {
         this.schedulable = list;
         this.scheduled = map;
         this.earliestTimes = times;
+        this.cost = cost;
+        this.nodesScheduled = map.size();
 
     }
 
@@ -49,7 +53,20 @@ public class AStarSchedule implements ISchedule {
         return copy;
     }
 
+    public int getCost(){
+        return cost;
+    }
+
+    public int getNodesScheduled(){
+        return nodesScheduled;
+    }
+
     public void scheduleNode(INode node, int processor) {
+        if(!this.schedulable.contains(node)){
+            throw new Error("cant schedule that node?");
+        }
+        this.schedulable.remove(node);
+        this.nodesScheduled++;
         Map<INode, Integer> parents = node.getParents();
         int latest = 0;
         for (Map.Entry<INode, Integer> parent : parents.entrySet()) {
@@ -64,5 +81,23 @@ public class AStarSchedule implements ISchedule {
         latest = Math.max(latest, this.earliestTimes[processor - 1]);
         this.scheduled.put(node, new Tuple<>(latest, processor));
         this.earliestTimes[processor - 1] = latest + node.getProcessingCost();
+        cost = Math.max(earliestTimes[processor - 1],cost);
+        updateSchedule(node);
+    }
+
+    private void updateSchedule(INode node){
+        Map<INode,Integer> children = node.getChildren();
+        for(INode child:children.keySet()){
+            boolean canAdd = true;
+            for(INode parent:child.getParents().keySet()){
+                if(!this.scheduled.containsKey(parent)){
+                    canAdd = false;
+                    break;
+                }
+            }
+            if(canAdd){
+                this.schedulable.add(child);
+            }
+        }
     }
 }
