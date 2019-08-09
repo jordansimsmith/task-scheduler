b4afbd09ef820326ddc67f3d1b29f1490e5a3dd2
Nidhinesh <nnidhinesh@gmail.com>
2019-08-09 15:33:27 +1200

    Implemented graphviz graph generator for input graph

--- src/main/java/task/scheduler/ui/InputGraphGenerator.java
+++ src/main/java/task/scheduler/ui/InputGraphGenerator.java
@@ -1,53 +1,75 @@
 package task.scheduler.ui;
+
+import guru.nidi.graphviz.engine.Format;
+import guru.nidi.graphviz.engine.Graphviz;
+import guru.nidi.graphviz.model.MutableGraph;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import task.scheduler.graph.IGraph;
+import task.scheduler.graph.INode;
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import org.graphstream.graph.*;
-import org.graphstream.graph.implementations.*;
+import static guru.nidi.graphviz.model.Factory.mutGraph;
+import static guru.nidi.graphviz.model.Factory.mutNode;
 
 
-import task.scheduler.graph.IGraph;
-import task.scheduler.graph.INode;
 
 public class InputGraphGenerator {
     private IGraph graph;
+    Image image;
 
     public InputGraphGenerator(IGraph input){
         this.graph = input;
     }
 
-    /**
-     * Generate graph generates the visualization of the graph from the provided IGraph
-     */
-    public void GenerateGraph(){
-        Graph visualization = new SingleGraph("Initial Graph");
+    public void generateGraph() {
+        MutableGraph g = mutGraph("example1").setDirected(true).use((gr, ctx) -> {
 
-        visualization.display();
-        visualization.setStrict(false);
-        visualization.setAutoCreate(true);
+            List<INode> nodes = graph.getNodes();
 
-        List<INode> nodes = graph.getNodes();
+            for (INode parent : nodes) {
+                Map<INode, Integer> children = parent.getChildren();
+                Set<INode> childrenList = children.keySet();
 
-        for (INode parent : nodes){
-            Map<INode, Integer> children = parent.getChildren();
-            Set<INode> childrenList = children.keySet();
+                for (INode child : childrenList) {
+                    mutNode(parent.getLabel()).addLink(mutNode(child.getLabel()));
 
-            for ( INode child : childrenList){
-                visualization.addEdge(parent.getLabel() + child.getLabel(), parent.getLabel(), child.getLabel());
-
+                }
             }
-        }
-        //Adding label
-        for (Node node : visualization) {
-            node.addAttribute("ui.label", node.getId());
-            node.addAttribute("ui.style", "text-size: 20px;");
-            node.addAttribute("ui.style", "size: 20px;");
-        }
 
+            //Edge case if there is only one node
+            if (nodes.size() == 1) {
+                mutNode(nodes.get(0).getLabel());
+            }
+        });
+
+        try {
+            Graphviz.fromGraph(g).width(200).render(Format.PNG).toFile(new File("resources/input-graph.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public IGraph getGraph() {
-        return graph;
+
+    public ImageView getGraph(){
+
+        generateGraph();
+
+        try {
+            image  = new Image(new FileInputStream("resources/input-graph.png"));
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return new ImageView(image);
     }
 }
