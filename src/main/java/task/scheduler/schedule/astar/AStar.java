ba28b27abe1b91b5665eff409a278e76f7552a8f
Harrison Leach <HarrisonL7190@gmail.com>
2019-08-13 14:51:46 +1200

    Add computation of bottom level cache and total graph weight

--- src/main/java/task/scheduler/schedule/astar/AStar.java
+++ src/main/java/task/scheduler/schedule/astar/AStar.java
@@ -5,15 +5,13 @@ import task.scheduler.graph.INode;
 import task.scheduler.schedule.ISchedule;
 import task.scheduler.schedule.IScheduler;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class AStar implements IScheduler {
     private PriorityQueue<AStarSchedule> solutions;
 
-    public static final int totalNodeWeighting = 0; // TODO: implement this
-    public static final Map<INode, Integer> bottomLevelCache = new HashMap<>(); // TODO: implement this
+    public static int totalNodeWeighting;
+    public static final Map<INode, Integer> bottomLevelCache = new HashMap<>();
 
     public AStar() {
 
@@ -21,6 +19,102 @@ public class AStar implements IScheduler {
 
     @Override
     public ISchedule execute(IGraph graph) {
-        throw new RuntimeException("not implemented");
+        totalNodeWeighting = getTotalNodeWeighting(graph);
+
+        bottomLevelCache.clear();
+        for (INode node : graph.getNodes()) {
+            computeCriticalPath(node, graph);
+        }
     }
+
+    private int getTotalNodeWeighting(IGraph graph) {
+        int totalNodeWeighting = 0;
+
+        for(INode node : graph.getNodes()) {
+            totalNodeWeighting += node.getProcessingCost();
+        }
+
+        return totalNodeWeighting;
+    }
+
+    private Map<INode, Integer> getBottomLevelCache(IGraph graph) {
+
+    }
+
+    /**
+     * computeCriticalPath is used to compute the greatest cost path from the provided source node.
+     * It updates the critical paths field with the maxmimal cost path.
+     * https://www.geeksforgeeks.org/find-longest-path-directed-acyclic-graph/
+     *
+     * @param source node to be considered
+     */
+    private void computeCriticalPath(INode source, IGraph graph) {
+        List<INode> nodes = graph.getNodes();
+
+        Map<INode, Integer> distances = new HashMap<>();
+        Map<INode, Boolean> visited = new HashMap<>();
+        Stack<INode> stack = new Stack<>();
+
+        // Initialise distances to all vertices as negative infinity
+        // distances to source node are zero
+        for (INode node : nodes) {
+            distances.put(node, Integer.MIN_VALUE);
+            visited.put(node, false);
+        }
+        distances.put(source, source.getProcessingCost());
+
+        // topologically sort nodes into the stack
+        for (INode node : nodes) {
+            if (!visited.get(node)) {
+                topologicalSortUtil(node, visited, stack);
+            }
+        }
+
+        // process nodes in topological order
+        while (!stack.empty()) {
+            INode node = stack.pop();
+
+            if (distances.get(node) != Integer.MIN_VALUE) {
+                // iterate over node children
+                for (INode child : node.getChildren().keySet()) {
+
+                    // update distances if greater
+                    if (distances.get(child) < distances.get(node) + child.getProcessingCost()) {
+                        distances.put(child, distances.get(node) + child.getProcessingCost());
+                    }
+                }
+            }
+        }
+
+        int max = 0;
+
+        for (int distance : distances.values()) {
+            max = Math.max(max, distance);
+        }
+
+        bottomLevelCache.put(source, max);
+    }
+
+    /**
+     * Topological sort recursive function
+     *
+     * @param node    current node
+     * @param visited map of whether nodes have been visited or not
+     * @param stack   stack to push topological ordering to
+     */
+    private void topologicalSortUtil(INode node, Map<INode, Boolean> visited, Stack<INode> stack) {
+        // mark current node as visited
+        visited.put(node, true);
+
+        // recursively call topological sort on unvisited nodes
+        for (Map.Entry<INode, Integer> child : node.getChildren().entrySet()) {
+            if (!visited.get(child.getKey())) {
+                topologicalSortUtil(child.getKey(), visited, stack);
+            }
+        }
+
+        stack.push(node);
+    }
+
+
 }
