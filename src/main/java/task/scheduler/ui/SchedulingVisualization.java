c0023e728466c14ef9cb84ef92f8b2e465453d34
Nidhinesh <nnidhinesh@gmail.com>
2019-08-14 08:04:22 +1200

    Created createNodeVisual helper class

---
+++ src/main/java/task/scheduler/ui/SchedulingVisualization.java
@@ -0,0 +1,56 @@
+package task.scheduler.ui;
+
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
+import javafx.scene.chart.Axis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.layout.StackPane;
+
+public class SchedulingVisualization<X, Y> extends XYChart<X, Y> {
+
+    public SchedulingVisualization(Axis<X> axisX, Axis<Y> axisY, ObservableList<Series<X, Y>> nodes) {
+        super(axisX, axisY);
+        setData(nodes);
+    }
+
+    @Override
+    protected void dataItemAdded(Series<X, Y> series, int i, Data<X, Y> data) {
+    }
+
+    @Override
+    protected void dataItemRemoved(Data<X, Y> data, Series<X, Y> series) {
+
+    }
+
+    @Override
+    protected void dataItemChanged(Data<X, Y> data) {
+
+    }
+
+    @Override
+    protected void seriesAdded(Series<X, Y> series, int i) {
+
+    }
+
+    @Override
+    protected void seriesRemoved(Series<X, Y> series) {
+
+    }
+
+    @Override
+    protected void layoutPlotChildren() {
+
+    }
+
+    private Node createNodeVisual(Data<X, Y> data){
+
+        Node container = data.getNode();
+
+        if (container == null){
+            container = new StackPane();
+            data.setNode(container);
+        }
+
+        return container;
+    }
+}
