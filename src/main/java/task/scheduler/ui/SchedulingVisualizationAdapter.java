06796d79f449f338a24bf582cfe97ba2a5e95785
Nidhinesh <nnidhinesh@gmail.com>
2019-08-14 22:30:46 +1200

    Broke up large method into smaller, more understandable methods

--- src/main/java/task/scheduler/ui/SchedulingVisualizationAdapter.java
+++ src/main/java/task/scheduler/ui/SchedulingVisualizationAdapter.java
@@ -2,6 +2,7 @@ package task.scheduler.ui;
 
 import javafx.collections.FXCollections;
 import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.Chart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.paint.Color;
@@ -10,17 +11,21 @@ import task.scheduler.graph.IGraph;
 import task.scheduler.graph.INode;
 import task.scheduler.schedule.ISchedule;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class SchedulingVisualizationAdapter {
 
     private static SchedulingVisualizationAdapter schedulingVisualizationAdapter = new SchedulingVisualizationAdapter();
-    Map<Integer, XYChart.Series> seriesMap = new HashMap<>();
 
-    private SchedulingVisualizationAdapter(){};
+    Map<Integer, XYChart.Series> seriesMap = new HashMap<>();
+    final NumberAxis xAxis = new NumberAxis();
+    final CategoryAxis yAxis = new CategoryAxis();
+    final SchedulingVisualization<Number,String> chart = new SchedulingVisualization<>(xAxis,yAxis);
+
+    private SchedulingVisualizationAdapter(){
+        setUpVisual();
+    };
 
     public static SchedulingVisualizationAdapter getInstance(){
         return schedulingVisualizationAdapter;
@@ -29,10 +34,28 @@ public class SchedulingVisualizationAdapter {
     public  SchedulingVisualization populateVisual(IGraph graph, ISchedule schedule){
         clearSeriesList();
 
-        final NumberAxis xAxis = new NumberAxis();
-        final CategoryAxis yAxis = new CategoryAxis();
+        for(INode node: graph.getNodes()) {
+            Tuple<Integer, Integer> nodeSchedule = schedule.getNodeSchedule(node);
 
-        final SchedulingVisualization<Number,String> chart = new SchedulingVisualization<>(xAxis,yAxis);
+            if (seriesMap.get(nodeSchedule.y) == null){
+                seriesMap.put(nodeSchedule.y, new XYChart.Series());
+            }
+            seriesMap.get(nodeSchedule.y).getData().add(new XYChart.Data(nodeSchedule.x, "Core" +  nodeSchedule.y, new SchedulingVisualization.DetailedInformation(node.getProcessingCost(), "status-red")));
+        }
+
+        for (XYChart.Series s : seriesMap.values()){
+            chart.getData().add(s);
+        }
+        chart.getStylesheets().add(getClass().getResource("ganttchart.css").toExternalForm());
+        return chart;
+    }
+
+
+    public Chart getChart(){
+        return this.chart;
+    }
+
+    private void setUpVisual(){
         xAxis.setLabel("");
         xAxis.setTickLabelFill(Color.CHOCOLATE);
         xAxis.setMinorTickCount(4);
@@ -44,24 +67,8 @@ public class SchedulingVisualizationAdapter {
         chart.setTitle("Scheduling");
         chart.setLegendVisible(false);
         chart.setBlockHeight( 50);
-
-        for(INode node: graph.getNodes()) {
-            Tuple<Integer, Integer> nodeSchedule = schedule.getNodeSchedule(node);
-
-            if (seriesMap.get(nodeSchedule.y) == null){
-                seriesMap.put(nodeSchedule.y, new XYChart.Series());
-            }
-
-            seriesMap.get(nodeSchedule.y).getData().add(new XYChart.Data(nodeSchedule.x, "Core" +  nodeSchedule.y, new SchedulingVisualization.DetailedInformation(node.getProcessingCost(), "status-red")));
-        }
-
-        for (XYChart.Series s : seriesMap.values()){
-            chart.getData().add(s);
-        }
-        chart.getStylesheets().add(getClass().getResource("ganttchart.css").toExternalForm());
-
-        return chart;
     }
+
 
     private void clearSeriesList(){
         for (XYChart.Series s : seriesMap.values()){
