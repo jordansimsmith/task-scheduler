fe15e465a3ffb0d07c61889898b118ae7f5f55a2
Nidhinesh <nnidhinesh@gmail.com>
2019-08-17 16:27:13 +1200

    Created wrapper class for node that would allow colour to be changed

---
+++ src/main/java/task/scheduler/ui/VisualNode.java
@@ -0,0 +1,44 @@
+package task.scheduler.ui;
+
+import task.scheduler.graph.INode;
+
+import java.util.Map;
+
+public class VisualNode implements INode {
+    INode node;
+    String colour;
+
+    public VisualNode(INode node){
+        this.node = node;
+    }
+
+    @Override
+    public Map<INode, Integer> getChildren() {
+        return node.getChildren();
+    }
+
+    @Override
+    public Map<INode, Integer> getParents() {
+        return node.getParents();
+    }
+
+    @Override
+    public int getProcessingCost() {
+        return node.getProcessingCost();
+    }
+
+    @Override
+    public String getLabel() {
+        return node.getLabel();
+    }
+
+    public INode getNode() {
+        return node;
+    }
+
+    public void setNode(INode node) {
+        this.node = node;
+    }
+
+
+}
